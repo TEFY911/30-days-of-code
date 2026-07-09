0dca096ad1b6d07bdb4f4cacd1b4b950858dc43d
TEFY911 <elcazacelulas@gmail.com>
2026-07-09 20:01:23 +0000

    chore: remove build artifacts from tracking; add .gitignore

--- HackerRank 30 Days/day 0/main.js
+++ HackerRank 30 Days/day 0/main.js
@@ -2,3 +2,16 @@ function processData(inputString) {
     console.log("Hello, World.");
     console.log(inputString);
 }
+
+process.stdin.resume();
+process.stdin.setEncoding("utf8");
+
+let inputString = "";
+
+process.stdin.on("data", function(input) {
+    inputString += input;
+});
+
+process.stdin.on("end", function() {
+    processData(inputString.trim());
+});
